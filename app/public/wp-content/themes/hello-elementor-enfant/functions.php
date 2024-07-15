f27ddd526159c5c6aa36ad585789a45c921497e3
thomaslapOPCR <114487960+thomaslapOPCR@users.noreply.github.com>
2024-07-15 12:32:30 +0200

    modification de la fonction php pur gerer le lien admin

--- app/public/wp-content/themes/hello-elementor-enfant/functions.php
+++ app/public/wp-content/themes/hello-elementor-enfant/functions.php
@@ -5,24 +5,17 @@ function wpchild_enqueue_styles(){
   wp_enqueue_style( 'wpm-elementor-style', get_template_directory_uri() . '/style.css' );
 }
 
+
 function add_admin_link_to_menu($items, $args) {
+    if (is_user_logged_in() && ($args->theme_location === 'primary' || $args->menu_id === 'ast-hf-mobile-menu')) {
+        $class = ($args->menu_id === 'ast-hf-mobile-menu') ? 'menu-item menu-item-type-post_type menu-item-object-page' : '';
+        $admin_link = '<li id="menu-item-22" class="menu-item-22 menu-item ' . $class . '"><a class="menu-link" href="' . admin_url() . '">Admin</a></li>';
 
-	if (is_user_logged_in() && $args->theme_location === 'primary') {
-		
-		$admin_link = '<li id="menu-item-22" class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
-		
-		
-		$items_array = explode('</li>', $items);
-		
-		
-		array_splice($items_array, 1, 0, $admin_link);
-  
-		
-		$items = implode('</li>', $items_array);
-	}
-	return $items;
-  }
+        $items_array = explode('</li>', $items);
+        array_splice($items_array, 1, 0, $admin_link);
+        $items = implode('</li>', $items_array);
+    }
+    return $items;
+}
 
-
-  add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
-  
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
